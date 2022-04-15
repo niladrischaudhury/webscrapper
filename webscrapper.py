36ee819d4c2e921b62fab08d975c01e1a834d8cd
niladrischaudhury <niladri.schaudhury@gmail.com>
2022-04-15 15:07:24 +0530

    first commit

---
+++ webscrapper.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
